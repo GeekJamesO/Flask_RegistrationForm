f5fd10b731eada1af9924d191262a3bcd24e79f4
JamesORourke <geekjamesorourkejr@icloud.com>
2017-08-08 19:23:39 -0700

    add Flash message categories, and color for those

--- RegistrationForm.py
+++ RegistrationForm.py
@@ -21,48 +21,46 @@ def  process():
 
     if (len(email) < 1):
         validated = False
-        flash("email must not be blank!")
+        flash("email must not be blank!", "error")
     elif not EMAIL_REGEX.match(email):
         validated = False
-        flash("'{}' is an invalid Email Address!".format(email))
+        flash("'{}' is an invalid Email Address!".format(email), "error")
     else:
         print "Email is good"
 
     if (len(first_name) < 1):
         validated = False
-        flash("First Name must not be blank!")
+        flash("First Name must not be blank!", "error")
     elif (any(char.isdigit() for char in first_name)):
         validated = False
-        flash("'{}' numbers are not allowed in first name!".format(first_name))
+        flash("'{}' numbers are not allowed in first name!".format(first_name), "error")
     else:
         print "First Name is good"
 
     if (len(last_name) < 1):
         validated = False
-        flash("Last Name must not be blank!")
+        flash("Last Name must not be blank!", "error")
     elif (any(char.isdigit() for char in last_name)):
         validated = False
-        flash("'{}' numbers are not allowed in last name!".format(last_name))
+        flash("'{}' numbers are not allowed in last name!".format(last_name), "error")
     else:
         print "Last Name is good"
 
     if (len(password1) < 8):
         validated = False
-        flash("First Password must not be atleast 8 characters!")
+        flash("First Password must not be atleast 8 characters!", "error")
 
     if (len(password2) < 8):
         validated = False
-        flash("Second Password must not be atleast 8 characters!")
+        flash("Second Password must not be atleast 8 characters!", "error")
 
     if (password1 != password2):
         validated = False
-        flash("Passwords do not match!")
+        flash("Passwords do not match!", "error")
 
     if (validated):
         return render_template('Success.html')
     else:
         return render_template('index.html')
-
-
 
 app.run(debug=True)
