7d11220c2c961f416df75d60a6d3a5c802faf7c3
JamesORourke <geekjamesorourkejr@icloud.com>
2017-08-08 22:21:15 -0700

    Added dates, improvements for all but date comparison

--- RegistrationForm.py
+++ RegistrationForm.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, flash
 import re
+from datetime import date, datetime
 
 app = Flask(__name__)
 app.secret_key = "ThisSoundBeSuperSpecialAndSecret"
@@ -14,51 +15,79 @@ def  process():
     email = request.form["email"]
     first_name = request.form['first_name']
     last_name = request.form['last_name']
+    birthdate = request.form['birthdate']
     password1 = request.form['password1']
     password2 = request.form['password2']
 
-    validated = True
+    isValid = True
 
     if (len(email) < 1):
-        validated = False
+        isValid = False
         flash("email must not be blank!", "error")
     elif not EMAIL_REGEX.match(email):
-        validated = False
+        isValid = False
         flash("'{}' is an invalid Email Address!".format(email), "error")
     else:
         print "Email is good"
 
     if (len(first_name) < 1):
-        validated = False
+        isValid = False
         flash("First Name must not be blank!", "error")
     elif (any(char.isdigit() for char in first_name)):
-        validated = False
+        isValid = False
         flash("'{}' numbers are not allowed in first name!".format(first_name), "error")
     else:
         print "First Name is good"
 
     if (len(last_name) < 1):
-        validated = False
+        isValid = False
         flash("Last Name must not be blank!", "error")
     elif (any(char.isdigit() for char in last_name)):
-        validated = False
+        isValid = False
         flash("'{}' numbers are not allowed in last name!".format(last_name), "error")
     else:
         print "Last Name is good"
 
+    today = date.today()
+
+    print birthdate, "  birthdate"
+    # x = datetime.datetime.strptime("2015-01-30", "%Y-%m-%d").strftime("%d-%m-%Y")
+    # print "date is ", x
+    if(len(birthdate) == 0 ):
+        isValid = False
+        flash("Birthdate must not be empty!", "error")
+    # elif(datetime(birthdate) < today):
+    #     isValid = False
+    #     flash("Birthdate must not be before today!", "error")
+    else:
+        print "Birthdate is good"
+
     if (len(password1) < 8):
-        validated = False
+        isValid = False
         flash("First Password must not be atleast 8 characters!", "error")
+    elif (not any(char.isdigit() for char in password1)):
+        isValid = False
+        flash("Passwords are required to have atleast one digit!", "error")
+    elif (not any(char.isalpha() for char in password1)):
+        isValid = False
+        flash("Passwords are required to have atleast one letter!", "error")
+
 
     if (len(password2) < 8):
-        validated = False
+        isValid = False
         flash("Second Password must not be atleast 8 characters!", "error")
+    elif (not any(char.isdigit() for char in password2)):
+        isValid = False
+        flash("Passwords are required to have atleast one digit!", "error")
+    elif (not any(char.isalpha() for char in password2)):
+        isValid = False
+        flash("Passwords are required to have atleast one letter!", "error")
 
     if (password1 != password2):
-        validated = False
+        isValid = False
         flash("Passwords do not match!", "error")
 
-    if (validated):
+    if (isValid):
         return render_template('Success.html')
     else:
         return render_template('index.html')
